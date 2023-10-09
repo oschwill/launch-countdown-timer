d66d0776bd633a39ef89d6c5398ce68aaff80186
Oliver Schwill <o.schwill@web.de>
2023-10-09 13:03:15 +0200

    animate the letters and mobile version

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,12 @@ const output = document.querySelector('.output');
 const topLetter = document.querySelectorAll('.top-letter');
 const bottomLetter = document.querySelectorAll('.bottom-letter');
 
+/* PROPERTIES */
 let countDownDate = new Date('Feb 7, 2024 00:00:00').getTime();
 let milliSecond = 1000;
-let checkChange = { x: 10 };
+let animSecond = 250;
+let animSecondDelay = 750;
+let topLetterContainer, bottomLetterContainer;
 
 // IFEE
 (() => {
@@ -25,28 +28,89 @@ let checkChange = { x: 10 };
       output.innerHTML = 'COUNTDOWN FINISHED';
     }
 
-    let days = Math.floor(countdown / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((countdown % (1000 * 60)) / 1000);
+    let timeObj = {
+      days: Math.floor(countdown / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      minutes: Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60)),
+      seconds: Math.floor((countdown % (1000 * 60)) / 1000),
+    };
 
-    // animate the letters
-    animateLetters([days, hours, minutes, seconds]);
+    // start animate letters
+    animateLetters(timeObj);
 
-    day.forEach((d) => (d.innerText = String(days).length > 1 ? days : ('0' + days).slice(-2)));
-    hour.forEach((d) => (d.innerText = String(hours).length > 1 ? hours : ('0' + hours).slice(-2)));
+    // paste values in html
+    day.forEach(
+      (d) =>
+        (d.innerText =
+          String(timeObj.days).length > 1 ? timeObj.days : ('0' + timeObj.days).slice(-2))
+    );
+    hour.forEach(
+      (d) =>
+        (d.innerText =
+          String(timeObj.hours).length > 1 ? timeObj.hours : ('0' + timeObj.hours).slice(-2))
+    );
     minute.forEach(
-      (d) => (d.innerText = String(minutes).length > 1 ? minutes : ('0' + minutes).slice(-2))
+      (d) =>
+        (d.innerText =
+          String(timeObj.minutes).length > 1 ? timeObj.minutes : ('0' + timeObj.minutes).slice(-2))
     );
     second.forEach(
-      (d) => (d.innerText = String(seconds).length > 1 ? seconds : ('0' + seconds).slice(-2))
+      (d) =>
+        (d.innerText =
+          String(timeObj.seconds).length > 1 ? timeObj.seconds : ('0' + timeObj.seconds).slice(-2))
     );
   }, milliSecond);
 })();
 
-const animateLetters = (timeArr) => {
-  checkChange.watch('x', function (id, oldVal, newVal) {
-    console.log(id + ' changed from ' + oldVal + ' to ' + newVal);
-  });
-  console.log(timeArr);
+const animateLetters = ({ days, hours, minutes, seconds }) => {
+  // animate Seconds
+  animater(topLetterContainer.seconds, bottomLetterContainer.seconds);
+  switch (true) {
+    case seconds === 0 && minutes > 0:
+      // Animate minutes
+      animater(topLetterContainer.minutes, bottomLetterContainer.minutes);
+      break;
+    case seconds === 0 && minutes === 0 && hours > 0:
+      // Animate hours and minutes
+      animater(topLetterContainer.minutes, bottomLetterContainer.minutes);
+      animater(topLetterContainer.hours, bottomLetterContainer.hours);
+      break;
+    case seconds === 0 && minutes === 0 && hours === 0:
+      // Animate days, hours and minutes
+      animater(topLetterContainer.minutes, bottomLetterContainer.minutes);
+      animater(topLetterContainer.hours, bottomLetterContainer.hours);
+      animater(topLetterContainer.days, bottomLetterContainer.days);
+      break;
+    default:
+      break;
+  }
 };
+
+const init = () => {
+  topLetterContainer = {
+    days: topLetter[0],
+    hours: topLetter[1],
+    minutes: topLetter[2],
+    seconds: topLetter[3],
+  };
+
+  bottomLetterContainer = {
+    days: bottomLetter[0],
+    hours: bottomLetter[1],
+    minutes: bottomLetter[2],
+    seconds: bottomLetter[3],
+  };
+};
+
+const animater = (topLetter, bottomLetter) => {
+  topLetter.animate(
+    { transform: 'rotateX(360deg)' },
+    { duration: animSecond, iterations: 1, delay: animSecondDelay }
+  );
+  bottomLetter.animate(
+    { transform: 'rotateX(-360deg)' },
+    { duration: animSecond, iterations: 1, delay: animSecondDelay }
+  );
+};
+
+init();
