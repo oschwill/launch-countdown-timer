01a8df4602d29c3fd1e1d5f0cef1410f70adcd0e
Olli1982 <granatenwerfer@web.de>
2023-10-09 08:47:10 +0200

    refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,12 @@ const second = document.querySelectorAll('.second');
 
 /* CONTAINER */
 const output = document.querySelector('.output');
+const topLetter = document.querySelectorAll('.top-letter');
+const bottomLetter = document.querySelectorAll('.bottom-letter');
 
 let countDownDate = new Date('Feb 7, 2024 00:00:00').getTime();
 let milliSecond = 1000;
+let checkChange = { x: 10 };
 
 // IFEE
 (() => {
@@ -17,7 +20,7 @@ let milliSecond = 1000;
 
     let countdown = countDownDate - now;
 
-    if (countdown < 1) {
+    if (countdown < milliSecond) {
       clearInterval(timer);
       output.innerHTML = 'COUNTDOWN FINISHED';
     }
@@ -26,6 +29,9 @@ let milliSecond = 1000;
     let hours = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((countdown % (1000 * 60)) / 1000);
+
+    // animate the letters
+    animateLetters([days, hours, minutes, seconds]);
 
     day.forEach((d) => (d.innerText = String(days).length > 1 ? days : ('0' + days).slice(-2)));
     hour.forEach((d) => (d.innerText = String(hours).length > 1 ? hours : ('0' + hours).slice(-2)));
@@ -37,3 +43,10 @@ let milliSecond = 1000;
     );
   }, milliSecond);
 })();
+
+const animateLetters = (timeArr) => {
+  checkChange.watch('x', function (id, oldVal, newVal) {
+    console.log(id + ' changed from ' + oldVal + ' to ' + newVal);
+  });
+  console.log(timeArr);
+};
